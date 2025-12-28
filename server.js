ed9c5b653c525fde8956fed499cfc6edfb60c241
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2025-12-28 23:37:37 +0700

    using framework express.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const { createServer } = require('http');
+const express = require('express')
+const app = express()
+const port = 9090
 
-const hostname = 'localhost';
-const port = 9090;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World \n  Thanh dat');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
