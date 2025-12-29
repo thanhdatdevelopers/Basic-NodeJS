c4fb403ed36f7eedb9f4ecf29d25a38ddd57addc
DatNT <datnt1@codluck.com>
2025-12-29 16:51:02 +0700

    Update route

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/route', (req, res) => {
+  res.send('Check route')
+})
+
+app.get('/routes', (req, res) => {
+  res.send('<h1>Check routes</h1>')
+})
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
