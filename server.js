698849f9cc4d922a48e2226c742154272bb3bfdd
DatNT <datnt1@codluck.com>
2025-12-29 14:32:31 +0700

    Update server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const app = express()
 const port = 9090
 
@@ -7,5 +8,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
