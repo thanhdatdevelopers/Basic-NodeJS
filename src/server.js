bc139a745ccbe7e1d4993abf4f547e2d126d8d21
Thanh Dat Nguyen <thanhdat.developers@gmail.com>
2026-01-01 16:29:34 +0700

    config template engine ejs

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = 9090
+
+// config template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/route', (req, res) => {
-  res.send('Check route')
+  // res.send('Check route')
+  res.render('index.ejs')
 })
 
 app.get('/routes', (req, res) => {
